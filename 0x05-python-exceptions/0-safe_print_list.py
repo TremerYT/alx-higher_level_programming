29a34618b783fed9274eef0b42c95771751eccc0
Jeffery Mutuku <jefferymutuku283@gmail.com>
2023-11-20 20:37:23 +0300

    corrected

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             count = count + 1
         except IndexError:
-            pass
+            continue
         print("")
         return count
 
