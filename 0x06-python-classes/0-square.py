e4c0e7094e4d4d7edfac61435d12882144b02d40
Jeffery Mutuku <jefferymutuku283@gmail.com>
2023-11-21 19:52:09 +0300

    tasks

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""This defines a class square."""
+
+class square:
+    """This represents a square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""This Defines a class square."""
+
+class square:
+    """This represents a square."""
+
+    def __init__(self, square):
+        """ This initializes a new square"""
+
+        self.__square == square
