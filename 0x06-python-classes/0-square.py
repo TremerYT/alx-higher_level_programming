de90f420ffa68e7470d5f5ae3af0499ffdba87d3
Jeffery Mutuku <jefferymutuku283@gmail.com>
2023-11-21 20:59:46 +0300

    ghhh

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-"""This is an empty class square that defines a square"""
+"""class Square that defines a square"""
 
 
 class Square:
-    """This is an empty class square that defines a square"""
+    """class Square that defines a square"""
     pass
