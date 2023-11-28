033c377729e35ea157bf50108a90a17936c95716
jeffery Mutuku <jefferymutuku283@gmail.com>
2023-11-28 08:18:45 -0800

    task 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This function is to add integers"""
 
-if ((not isinstance(a, int) and not isinstance(a, float))):
-    raise TypeError("a must be an integer")
-if ((not isinstance(b, int) and not isinstance(b, float))):
-    raise TypeError("b must be an integer")
-return (int(a) + int(b))
+def add_integer(a, b=98):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
