79d12fdbfff713293d940280fc38569f3153e69c
Jeffery Mutuku <jefferymutuku283@gmail.com>
2023-11-30 21:27:48 +0300

    corrected

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,17 +5,11 @@ def matrix_divided(matrix, div):
     """
     This function divides a matrix with a number
     """
-    if not isinstance(matrix, list) or not \
-            all(isinstance(i, list) for i in matrix):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) \
-            of integers/floats"
-        )
+    if not isinstance(matrix, list) or not all(isinstance(i, list) for i in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     for i in matrix:
         if not all(isinstance(obj, (int, float)) for obj in i):
-            raise TypeError(
-                "matrix must be a matrix (list of lists) of integers/floats"
-            )
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if not all(len(i) == len(matrix[0]) for i in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
