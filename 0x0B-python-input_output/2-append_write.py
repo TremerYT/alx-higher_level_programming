bd2f58c2c1363802c638e6a604fa9f2e43a22bb2
Jeffery Mutuku <jefferymutuku283@gmail.com>
2023-12-05 21:10:23 +0300

    corrected

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""This defines a file-appending function"""
+"""Defines a file-appending function."""
 
 
 def append_write(filename="", text=""):
-    """This func appends a string to the end of a UTF8 text file.
+    """Appends a string to the end of a UTF8 text file.
 
     Args:
         filename (str): The name of the file to append to.
@@ -11,5 +11,5 @@ def append_write(filename="", text=""):
     Returns:
         The number of characters appended.
     """
-    wit open(filename, "a", encoding="utf-8") as f:
+    with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-# 6-from_json_string.py
-"""This defines a JSON-to-object function"""
-import json
+"""Defines a file-appending function."""
 
 
-def save_to_json_file(my_obj, filename):
-    """This returns the python object rep"""
-    return json.loads(my_str)
+def append_write(filename="", text=""):
+    """Appends a string to the end of a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
