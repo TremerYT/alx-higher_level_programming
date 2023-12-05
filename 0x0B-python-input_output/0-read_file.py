ad66a453141ef3770a154748ff28fdad9770c898
jeffery Mutuku <jefferymutuku283@gmail.com>
2023-12-05 10:00:03 -0800

    corrected

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This defines a text file-reading function"""
 
+
 def read_file(filename=""):
     """This prints the contents of the file to the stdout"""
     with open(filename, encoding="utf-8") as f:
