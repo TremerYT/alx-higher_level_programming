49c6a9268e4b42ee9780e88666fb1a429ec6384c
Jeffery mutuku <jefferymutuku283@gmail.com>
2024-02-18 07:24:38 -0800

    done correcter

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,14 +5,14 @@ import sys
 
 
 if __name__ == "__main__":
-    database = MySQLdb.connect(host="localhost", username=sys.argv[1],
-                         password=sys.argv[2], database=sys.argv[3], port=3306)
-    cursor = database.cursor()
-    cursor.execute("""SELECT cities.name FROM
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM
                 cities INNER JOIN states ON states.id=cities.state_id
                 WHERE states.name=%s""", (sys.argv[4],))
-    rows = cursor.fetchall()
+    rows = cur.fetchall()
     tmp = list(row[0] for row in rows)
     print(*tmp, sep=", ")
-    cursor.close()
-    database.close()
+    cur.close()
+    db.close()
